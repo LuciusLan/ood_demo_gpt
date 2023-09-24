9c6e7e7872bca26f63c4d6f877fb817b1d55ce69
LuciusLan <cifellan@hotmail.com>
2023-09-24 23:30:13 +0800

    proto for gpt query

--- main.py
+++ main.py
@@ -9,12 +9,14 @@ from tqdm import tqdm
 from tools import Scorer
 
 LEN_THRES = 100
-SOURCE_CORPUS = './data/small/book/set1_text.txt'
-TARGET_CORPUS = './data/small/beauty/set1_text.txt'
+SOURCE_CORPUS = './data/book.labeled.sent.txt'
+TARGET_CORPUS = './data/beauty.labeled.sent.txt'
+SOURCE_LABEL = './data/book.labeled.label.txt'
+TARGET_LABEL = './data/beauty.labeled.label.txt'
 
 # Import our models. The package will take care of downloading the models automatically
-tokenizer = AutoTokenizer.from_pretrained("princeton-nlp/unsup-simcse-roberta-large")
-model = AutoModel.from_pretrained("princeton-nlp/unsup-simcse-roberta-large")
+tokenizer = AutoTokenizer.from_pretrained("princeton-nlp/unsup-simcse-roberta-large", cache_dir='D:\\Dev\\LM')
+model = AutoModel.from_pretrained("princeton-nlp/unsup-simcse-roberta-large", cache_dir='D:\\Dev\\LM')
 
 scorer = Scorer('princeton-nlp/unsup-simcse-roberta-large')
 
@@ -27,6 +29,14 @@ with open (SOURCE_CORPUS, 'r') as f:
 with open (TARGET_CORPUS, 'r') as f:
     query_texts = f.readlines()
 
+with open (SOURCE_LABEL, 'r') as f:
+    source_labels = f.readlines()
+
+with open (TARGET_LABEL, 'r') as f:
+    query_labels = f.readlines()
+
+f.close()
+
 
 scorer.build_index(source_texts)
 
@@ -34,16 +44,18 @@ query_w_demos = []
 for query_id, q_sent in tqdm(enumerate(query_texts), total=len(query_texts)):
     if len(q_sent.split(' ')) > LEN_THRES:
         continue
-    temp = {'qid': query_id}
-    results = scorer.search(q_sent, threshold=0, top_k=5)
+    temp = {'qid': query_id, 'q_sent': q_sent, 'q_label': query_labels[query_id]}
+    results = scorer.search(q_sent, threshold=0, top_k=-5)
     index = []
     for i in results:
         index.append(source_texts.index(i[0]))
         # if i[1] > 0.5:
         #     print (query_sents.index(q_sent),q_sent, i)
     # print (index)
-    temp.update({'sid': index})
+    source = [{'sid': si, 's_sent': source_texts[si], 's_label': source_labels[si]} for si in index]
+    temp.update({'source': source})
     query_w_demos.append(temp)
+
 
 # 0 for neg, 1 for neutral, 2 for pos
 print()
---
+++ run_gpt.py
@@ -0,0 +1,124 @@
+from datetime import datetime
+import time
+import json
+import sys
+import random
+import pdb 
+from pathlib import Path
+import pickle
+import re
+
+from tqdm import tqdm
+import openai
+
+# SET OPENAI API KEY
+
+MODEL = 'gpt-3.5-turbo'
+
+def completePrompt(p, model, instruction):
+    response = openai.Completion.create(
+        model=model,
+        prompt = instruction + p + "\n\n",
+        temperature=0.7,
+        max_tokens=256,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0
+    )
+    return(response.choices[0].text)
+
+def doQuery(p, model, instruction, ans):
+    sysout = completePrompt(p, model, instruction)
+    sysout = sysout.strip()
+    print(p, "System:", sysout)
+    sysout = sysout[:len(ans)]
+    return(sysout == ans)
+
+def chatCompletPrompt(p, model):
+    try:
+        response = openai.ChatCompletion.create(
+            model=model,
+            messages=[
+                {"role": "system", "content": "You are a helpful assistant. You are to be asked to answer on what sentiment is expressed in a sentence."},
+                {"role": "user", "content": p}
+            ],
+            max_tokens=5,
+            request_timeout=30
+        )
+    except TypeError:
+        print()
+    except openai.error.Timeout:
+        response = openai.ChatCompletion.create(
+            model=model,
+            messages=[
+                {"role": "system", "content": "You are a helpful assistant. You are to be asked to answer on what sentiment is expressed in a sentence."},
+                {"role": "user", "content": p}
+            ],
+            max_tokens=5,
+            request_timeout=30
+        )
+    except openai.error.RateLimitError:
+        print()
+    #print(response.choices[0].message)
+    return response.choices[0].message.content
+
+def doChatQuery(p, model, ans):
+    time.sleep(0.5)
+    sysout = chatCompletPrompt(p, model)
+    sysout = sysout.strip()
+    #print(p, "System:", sysout)
+    try:
+        sysout = re.search(r'Label:\s?(\w+)', sysout, re.I).groups()[0]
+    except TypeError:
+        sysout = 'x'
+    return(sysout == ans)
+
+# results File: runID - # dd/mm/YY H:M:S
+dt_string = datetime.now().strftime("%d%m%Y_%H%M%S")
+
+# CREATE RESULTS FILE
+resFile = Path("runs") / dt_string
+resFile.touch(exist_ok=False)
+resFile.write_text("File,Iteration,Total,VPE Correct,NO VPE Correct\n")
+
+def label2text(label):
+    if label == '0\n':
+        return 'negative'
+    elif label == '1\n':
+        return 'neutral'
+    elif label == '2\n':
+        return 'positive'
+
+# RUN ITERATIONS
+for iteration in range(1):
+    print("STARTING ITERATION", iteration, "="*30)
+
+    # RUN THROUGH EXAMPLE FILES
+        
+    with open('book2beauty_bot5.pkl', 'rb') as source:
+        examples = pickle.load(source)
+
+        print("Source: BOOK, Target: BEAUTY")
+        
+        examples = random.sample(examples, 500)
+
+        correct = 0
+
+        # RUN THROUGH EXAMPLES
+        for j, e in tqdm(enumerate(examples), total=len(examples)):
+
+            prefix = f"Given the input sentence, assign a sentiment label from ['positive', 'neutral', 'negative']. Give your response with the answer label only. Do not include irrelevant text. \nFollowing are a few demonstrations on how to assign the sentiment label:\n"
+            demo_text = ''
+            for demo in e['source']:
+                demo_text += f'Sentence: {demo["s_sent"]}Label:{ label2text(demo["s_label"])}\n'
+            prompt = prefix + demo_text + f"Now assign sentiment label for the following sentence. Start your answer with \"Label:\". Sentence:\n{e['q_sent']}"
+            answer = label2text(e['q_label'])
+            #res = doQuery(prompt, ARGS.model, instructions, answer)
+            res = doChatQuery(prompt, MODEL, answer)
+
+            if res is True:
+                correct += 1
+
+        with resFile.open("a") as f:
+            f.write(f'Accuracy: {correct/500}')
+    print(f"iteration {iteration}")
